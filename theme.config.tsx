e41a7fdf44a6b1b96453b5b558721c4ae1c230bd
Juan31416 <j.pabloesza98@gmail.com>
2023-03-03 14:05:24 -0800

    refactor: change the blog theme to a docs theme

---
+++ theme.config.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: (
+    <span>
+      J<strong>π</strong> Notes
+    </span>
+  ),
+  project: {
+    link: "https://github.com/Juan31416/jp-notebook",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  footer: {
+    text: "Nextra Docs Template",
+  },
+};
+
+export default config;
